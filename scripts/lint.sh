1ac7275061e3adf9f51cff204fc349935b803821
Harrison Chase <hw.chase.17@gmail.com>
2019-11-12 16:00:51 -0500

    cr

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-err=0
-trap 'err=1' ERR
+set -eu
 pylint --rcfile=.pylintrc bubs
 pydocstyle --config=.pydocstyle_test bubs
 pydocstyle --config=.pydocstyle bubs
 isort --recursive --diff bubs
 flake8 --config=setup.cfg bubs
-test $err = 0 # Return non-zero if any command failed
